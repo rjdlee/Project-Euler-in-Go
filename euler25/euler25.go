b3a577fac6fb2e2d3929c4ab557b57f0360f770b
rjdlee <rijdlee@gmail.com>
2014-09-16 19:54:01 -0400

    Reverted
    Need to work on handling large numbers, tried big

--- euler25/euler25.go
+++ euler25/euler25.go
@@ -2,35 +2,25 @@
 //
 // Output:
 // 4179871
-// Runtime:  552.135279ms
-
+// Runtime: 552.135279ms
 package main
 
 import (
 	"fmt"
 	"math"
-	"math/big"
 	"time"
 )
 
-const n float64 = 4782
-const decimalPlaces float64 = 10000
+const n float64 = 4762
 
 func main() {
 	t0 := time.Now()
 
-	phi := big.NewInt(int64(math.Floor(math.Phi * decimalPlaces)))
-	phi.Exp(phi, big.NewInt(int64(n)), big.NewInt(0))
+	sqrt5 := math.Sqrt(5)
+	phi := (1 + sqrt5) / 2
+	phiPow := math.Pow(phi, n)
 
-	sqrt5 := big.NewInt(int64(math.Floor(math.Sqrt(5) * decimalPlaces)))
-
-	num := big.NewInt(int64(math.Pow(-1*10000, n))) // (-1)^n
-	num.Div(num, phi)
-	num.Sub(phi, num)
-	num.Div(num, sqrt5)
-	num.Div(num, big.NewInt(int64(math.Pow(10000, n*n))))
-	fmt.Println(num)
-
+	fmt.Println((phiPow - (math.Pow(float64(-1), n) / phiPow)) / sqrt5)
 	t1 := time.Now()
 	fmt.Println("Runtime: ", t1.Sub(t0))
 }
